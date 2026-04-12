bce1ddd7cd5399860f8e1c7d14987580dfc0c03b
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-04-12 14:12:20 -0300

    feat: initialize project with React, GSAP, and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>;
+};
+
+export default App;
