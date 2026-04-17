84faf6e76c9c9af55f21347b25cd49a2d258ec1b
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-04-17 11:33:24 -0300

    feat: add NavBar and related types

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 
+import NavBar from './components/NavBar';
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-screen">
-      <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
-    </div>
+    <main>
+      <NavBar />
+    </main>
   );
 };
 
