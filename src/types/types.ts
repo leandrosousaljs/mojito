02fa8a1402ce75f689ea3185d5becb6a21943155
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-04-21 12:01:40 -0300

    feat: add hero section fist part

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,17 +1,17 @@
 export interface NavLink {
-	id: string;
-	title: string;
+  id: string;
+  title: string;
 }
 
 export interface DrinkItem {
-	name: string;
-	country: string;
-	detail: string;
-	price: string;
+  name: string;
+  country: string;
+  detail: string;
+  price: string;
 }
 
 export interface ProfileItem {
-	imgPath: string;
+  imgPath: string;
 }
 
 export type FeatureItem = string;
@@ -19,31 +19,31 @@ export type FeatureItem = string;
 export type GoodItem = string;
 
 export interface StoreContact {
-	phone: string;
-	email: string;
+  phone: string;
+  email: string;
 }
 
 export interface StoreInfo {
-	heading: string;
-	address: string;
-	contact: StoreContact;
+  heading: string;
+  address: string;
+  contact: StoreContact;
 }
 
 export interface OpeningHour {
-	day: string;
-	time: string;
+  day: string;
+  time: string;
 }
 
 export interface SocialItem {
-	name: string;
-	icon: string;
-	url: string;
+  name: string;
+  icon: string;
+  url: string;
 }
 
 export interface SliderItem {
-	id: number;
-	name: string;
-	image: string;
-	title: string;
-	description: string;
+  id: number;
+  name: string;
+  image: string;
+  title: string;
+  description: string;
 }
